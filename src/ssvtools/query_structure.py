db683ae3c40ae1c8ecbe2e1e73144352d0cfa821
Richard Christie <r.christie@auckland.ac.nz>
2025-06-18 16:38:55 +1200

    Separate getting vagus structure and evaluating branch coordinates

--- src/ssvtools/query_structure.py
+++ src/ssvtools/query_structure.py
@@ -5,7 +5,7 @@ including trunk information, branch positions and orientations, and level marker
 from cmlibs.maths.vectorops import normalize
 from cmlibs.utils.zinc.field import get_group_list
 from cmlibs.zinc.field import Field
-from cmlibs.zinc.region import Region
+from cmlibs.zinc.fieldmodule import Fieldmodule
 import logging
 import re
 
@@ -15,16 +15,16 @@ logger = logging.getLogger(__name__)
 
 # following keyword is present in names of all annotations marking part of the trunk
 trunk_keyword = 'vagus nerve'
-# exception
+# exception for branches containing the following text:
 non_trunk_keyword = 'of vagus nerve'
 # following keywords are part of names of all annotations marking branches (without trunk_keywords)
 branch_keywords = ['branch', 'nerve']
 
 
-def get_trunk_group(fieldmodule):
+def get_vagus_trunk_group(fieldmodule: Fieldmodule):
     """
     Get the Zinc Group containing the vagus trunk part of the model.
-    :param fieldmodule: Fieldmodule for a Zinc Region
+    :param fieldmodule: Fieldmodule of Zinc Region containing subject-specific-vagus nerve scaffold.
     :return: Zinc Group for left/right vagus nerve or None if none.
     """
     for side in ('left', 'right'):
@@ -37,31 +37,19 @@ def get_trunk_group(fieldmodule):
     return None
 
 
-def query_branches(region: Region):
+def get_vagus_structure_maps(fieldmodule: Fieldmodule):
     """
-    Get information about position and orientation of branches off the vagus trunk.
-    :param region: Zinc Region containing SPARC subject-specific vagus scaffold (EX file read into region).
-    :return: dict mapping from branch name to start (vagus coordinates, body direction left, anterior, down)
-    excluding common branches, dict mapping common branch names to list of variant branch names within them.
+    Get the structure of the nerve:
+    1. map of all nerve tree trunk/single branch group names to parent trunk/branch group name (or None) and
+    list of child branch group names (can be empty).
+    2. map of all common branch names to the list of variant single branch names A, B, C etc.
+    :param fieldmodule: Fieldmodule of Zinc Region containing subject-specific-vagus nerve scaffold.
+    :return: dict group name -> (parent group name, list of child group names),
+    dict common group name -> list of variant group names.
     """
-    fieldmodule = region.getFieldmodule()
-    # coordinates = fieldmodule.findFieldByName('coordinates').castFiniteElement()
-    straight_coordinates = fieldmodule.findFieldByName('straight coordinates').castFiniteElement()
-    vagus_coordinates = fieldmodule.findFieldByName('vagus coordinates').castFiniteElement()
-    mesh3d = fieldmodule.findMeshByDimension(3)
-    derivative_xi1 = mesh3d.getChartDifferentialoperator(1, 1)
-    nodes = fieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
-    trunk_group = get_trunk_group(fieldmodule)
-    trunk_nodeset_group = trunk_group.getNodesetGroup(nodes)
-    if not trunk_group:
-        logger.error('query_branches: Missing vagus trunk group')
-        return {}
-    # all branches and other annotations are groups within the region
-    # determine what is a branch by name matching.
-    branch_info = {}
     group_list = get_group_list(fieldmodule)
-    # vagus scaffolds have common groups for all branches with variant A/B/C names
-    # first determine these groups; a map from common group name to list of variant group names
+    # vagus scaffolds have common groups for all branches with variant names containing letters A, B, C, etc.
+    # first determine these groups as a map from common group name to list of variant group names
     common_group_map = {}
     for group in group_list:
         group_name = group.getName()
@@ -72,30 +60,75 @@ def query_branches(region: Region):
                 variant_group_list = []
             variant_group_list.append(group_name)
             common_group_map[common_name] = variant_group_list
-    fieldcache = fieldmodule.createFieldcache()
+    trunk_group = get_vagus_trunk_group(fieldmodule)
+    mesh3d = fieldmodule.findMeshByDimension(3)
+    nodes = fieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+    coordinates = fieldmodule.findFieldByName("coordinates").castFiniteElement()
+    if not (trunk_group and coordinates.isValid()):
+        logger.warning('get_vagus_structure_maps: Missing vagus trunk group')
+        return {}, {}
+    trunk_group_name = trunk_group.getName()
+    group_nodeset_map = {trunk_group_name: trunk_group.getNodesetGroup(nodes)}
+    single_branch_groups = []
+    # map of group name to [parent group name, list of child group names]
+    structure_map = {trunk_group_name: [None, []]}
     for group in group_list:
         group_name = group.getName()
+        # exclude common groups
         if common_group_map.get(group_name):
             continue
+        # exclude trunk groups
         if (trunk_keyword in group_name) and not (non_trunk_keyword in group_name):
             continue
-        # branches have 3-D elements in them
-        mesh_group = group.getMeshGroup(mesh3d)
-        if (not mesh_group.isValid()) or (mesh_group.getSize() == 0):
+        # exclude non-matches to branch keywords
+        if not any(branch_keyword in group_name for branch_keyword in branch_keywords):
             continue
-        if any(branch_keyword in group_name for branch_keyword in branch_keywords):
-            element = mesh_group.createElementiterator().next()
-            eft = element.getElementfieldtemplate(vagus_coordinates, -1)
-            node = element.getNode(eft, 1)
-            if not trunk_nodeset_group.containsNode(node):
-                continue  # parent is not the trunk
-            # this is the centre of the branch at its root
-            fieldcache.setMeshLocation(element, [0.0, 0.5, 0.5])
-            # Note: 3rd component of material coordinate is unit coordinate down vagus trunk
-            _, branch_vagus_coordinate = vagus_coordinates.evaluateReal(fieldcache, 3)
-            _, d1 = straight_coordinates.evaluateDerivative(derivative_xi1, fieldcache, 3)
-            # branch direction axes are (left, anterior, down the body)
-            branch_direction = normalize(d1)
-            branch_info[group_name] = (branch_vagus_coordinate, branch_direction)
+        # exclude groups with no 3-D elements
+        if group.getMeshGroup(mesh3d).getSize() <= 0:
+            continue
+        single_branch_groups.append(group)
+        group_nodeset_map[group_name] = group.getNodesetGroup(nodes)
+        structure_map[group_name] = [None, []]
+    for group in single_branch_groups:
+        group_name = group.getName()
+        # the first element in each branch starts with 2 nodes in the parent branch
+        # whose parameters are general-linearly-mapped to get starting branch parameters
+        # determine parent group from this
+        element = group.getMeshGroup(mesh3d).createElementiterator().next()
+        eft = element.getElementfieldtemplate(coordinates, -1)
+        node = element.getNode(eft, 1)
+        parent_group_name = None
+        for compare_group_name, compare_nodeset_group in group_nodeset_map.items():
+            if compare_group_name == group_name:
+                continue
+            if compare_nodeset_group.containsNode(node):
+                parent_group_name = compare_group_name
+                structure_map[group_name][0] = parent_group_name
+                structure_map[parent_group_name][1].append(group_name)
+                break
+    return structure_map, common_group_map
 
-    return branch_info, common_group_map
+
+def evaluate_branch_start_coordinates(coordinate_field: Field, branch_group_names):
+    """
+    Evaluate the start coordinates and normalized (unit) direction of the listed branches.
+    :param coordinate_field: Zinc coordinate Field to evaluate.
+    :param branch_group_names: List of branch group names.
+    :return: list of (branch_group_names, start_x, start_direction)
+    """
+    fieldmodule = coordinate_field.getFieldmodule()
+    mesh3d = fieldmodule.findMeshByDimension(3)
+    derivative_xi1 = mesh3d.getChartDifferentialoperator(1, 1)
+    fieldcache = fieldmodule.createFieldcache()
+    branch_start_coordinates = []
+    for branch_group_name in branch_group_names:
+        group = fieldmodule.findFieldByName(branch_group_name).castGroup()
+        mesh_group = group.getMeshGroup(mesh3d)
+        element = mesh_group.createElementiterator().next()  # get the first element
+        # this is the centre of the branch at its root
+        fieldcache.setMeshLocation(element, [0.0, 0.5, 0.5])
+        _, start_x = coordinate_field.evaluateReal(fieldcache, 3)
+        _, start_d1 = coordinate_field.evaluateDerivative(derivative_xi1, fieldcache, 3)
+        direction = normalize(start_d1)
+        branch_start_coordinates.append((branch_group_name, start_x, direction))
+    return branch_start_coordinates
--- tests/test_query_structure.py
+++ tests/test_query_structure.py
@@ -1,6 +1,6 @@
 from cmlibs.zinc.context import Context
 from cmlibs.zinc.result import RESULT_OK
-from ssvtools.query_structure import query_branches
+from ssvtools.query_structure import get_vagus_structure_maps, evaluate_branch_start_coordinates
 from testutils import assertAlmostEqualList
 import os
 import unittest
@@ -21,25 +21,17 @@ class SSVToolsTestCase(unittest.TestCase):
         context = Context("Test")
         region = context.getDefaultRegion()
         self.assertEqual(RESULT_OK, region.readFile(data_file))
-        branch_info, common_branch_map = query_branches(region)
-        expected_branch_info = {
-            'left A thoracic cardiopulmonary branch of vagus nerve': (
-                [-0.0003328408968624165, 1.7493737182796838e-05, 0.3813658363063589],
-                [-0.9097938227559508, 0.38257762513656673, 0.16096447067591738]),
-            'left B thoracic cardiopulmonary branch of vagus nerve': (
-                [0.0010020650864467912, -0.001554513928071552, 0.40674113258208205],
-                [0.5530901132994802, -0.6180013793215057, 0.5587178373088555]),
-            'left superior laryngeal nerve': (
-                [0.0008767095326078595, 0.00045882124897275354, 0.1315331326187551],
-                [0.6981983439472732, 0.6737079331530234, -0.24215014622330203])}
-        self.assertEqual(len(branch_info), len(expected_branch_info))
-        TOL = 1.0E-8
-        for branch_name in branch_info.keys():
-            start_location, start_direction = branch_info[branch_name]
-            expected_start_location, expected_start_direction = expected_branch_info[branch_name]
-            assertAlmostEqualList(self, start_location, expected_start_location, TOL)
-            assertAlmostEqualList(self, start_direction, expected_start_direction, TOL)
-
+        fieldmodule = region.getFieldmodule()
+        structure_map, common_branch_map = get_vagus_structure_maps(fieldmodule)
+        expected_structure_map = {
+            'left vagus nerve': [None, ['left A thoracic cardiopulmonary branch of vagus nerve',
+                                        'left B thoracic cardiopulmonary branch of vagus nerve',
+                                        'left superior laryngeal nerve']],
+            'left A branch of superior laryngeal nerve': ['left superior laryngeal nerve', []],
+            'left A thoracic cardiopulmonary branch of vagus nerve': ['left vagus nerve', []],
+            'left B thoracic cardiopulmonary branch of vagus nerve': ['left vagus nerve', []],
+            'left superior laryngeal nerve': ['left vagus nerve', ['left A branch of superior laryngeal nerve']]}
+        self.assertEqual(expected_structure_map, structure_map)
         expected_common_branch_map = {
             'left branch of superior laryngeal nerve': [
                 'left A branch of superior laryngeal nerve'],
@@ -47,6 +39,27 @@ class SSVToolsTestCase(unittest.TestCase):
                 'left A thoracic cardiopulmonary branch of vagus nerve',
                 'left B thoracic cardiopulmonary branch of vagus nerve']}
         self.assertEqual(common_branch_map, expected_common_branch_map)
+        vagus_coordinates = fieldmodule.findFieldByName('vagus coordinates')
+        branch_start_coordinates = evaluate_branch_start_coordinates(
+            vagus_coordinates, structure_map['left vagus nerve'][1])
+        expected_branch_start_coordinates = [
+            ('left A thoracic cardiopulmonary branch of vagus nerve',
+                [-0.0003328408968624165, 1.7493737182796838e-05, 0.3813658363063589],
+                [-0.9106917613635638, 0.38295521752225525, 0.15487355215469165]),
+            ('left B thoracic cardiopulmonary branch of vagus nerve',
+                [0.0010020650864467912, -0.001554513928071552, 0.40674113258208205],
+                [0.5645932995214857, -0.630854592172089, 0.5322188362608271]),
+            ('left superior laryngeal nerve',
+                [0.0008767095326078595, 0.00045882124897275354, 0.1315331326187551],
+                [0.6662449900204912, 0.64287539363479, -0.3779270320200837])]
+        self.assertEqual(len(branch_start_coordinates), len(expected_branch_start_coordinates))
+        TOL = 1.0E-8
+        for i in range(len(branch_start_coordinates)):
+            expected_branch_name, expected_start_location, expected_direction = expected_branch_start_coordinates[i]
+            branch_name, start_location, direction = branch_start_coordinates[i]
+            self.assertEqual(branch_name, expected_branch_name)
+            assertAlmostEqualList(self, start_location, expected_start_location, TOL)
+            assertAlmostEqualList(self, direction, expected_direction, TOL)
 
 
 if __name__ == "__main__":
